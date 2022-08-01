b0b2b42fa456f4f15ceb22f9d2bc942490e0f766
Jonas Dujava <jonas.dujava@gmail.com>
2022-08-01 17:57:54 +0200

    initial keybindings

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -185,22 +185,24 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define CTRL ControlMask
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
-	/* mask                 keysym          function        argument */
-	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	/* mask           keysym          function          argument */
+	{ XK_ANY_MOD,     XK_Break,       sendbreak,        {.i =  0} },
+	{ ControlMask,    XK_Print,       toggleprinter,    {.i =  0} },
+	{ ShiftMask,      XK_Print,       printscreen,      {.i =  0} },
+	{ XK_ANY_MOD,     XK_Print,       printsel,         {.i =  0} },
+	{ TERMMOD,        XK_Up,          zoom,             {.f = +1} },
+	{ TERMMOD,        XK_Down,        zoom,             {.f = -1} },
+	{ CTRL,           XK_equal,       zoom,             {.f = +1} },
+	{ CTRL,           XK_minus,       zoom,             {.f = -1} },
+	{ CTRL,           XK_0,           zoomreset,        {.f =  0} },
+	{ ShiftMask,      XK_Insert,      selpaste,         {.i =  0} },
+	{ MODKEY,         XK_c,           clipcopy,         {.i =  0} },
+	{ MODKEY,         XK_v,           clippaste,        {.i =  0} },
+	{ TERMMOD,        XK_Num_Lock,    numlock,          {.i =  0} },
 };
 
 /*
