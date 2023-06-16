ec32eedbe7fe4807c3abe456140aebbd26064b4e
Jonas Dujava <jonas.dujava@gmail.com>
2023-06-16 12:10:03 +0200

    prettier selection colors

--- config.def.h
+++ config.def.h
@@ -143,7 +143,7 @@ static const char *colorname[] = {
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
-	"#555555",
+	"#2e3440",
 	"gray90", /* default foreground colour */
 	"black", /* default background colour */
 };
@@ -151,12 +151,17 @@ static const char *colorname[] = {
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor, reverse cursor, selection
  */
 unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 256;
+unsigned int selectionbg = 257;
+unsigned int selectionfg = 7;
+/* If 0 use selectionfg as foreground in order to have a uniform foreground-color */
+/* Else if 1 keep original foreground-color of each cell => more colors :) */
+static int ignoreselfg = 1;
 unsigned int bg = 259, bgUnfocused = 259;
 
 /*
